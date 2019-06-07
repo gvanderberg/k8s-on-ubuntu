ef059102c4bc63a7f3bde2347cc094dec739e585
Gareth van der Berg <gareth.shaun@gmail.com>
2019-06-07 10:26:47 +0200

    update script

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 apt-get update
-apt-get install apt-transport-https -y
 
 echo 1\) Install Docker
 
-apt-get install docker.io -y
+apt-get install -y docker.io
+systemctl start docker
+systemctl enable docker
 usermod support -aG docker
 
 echo 2\) Disable Swap
@@ -13,8 +14,9 @@ swapoff -a
 
 echo 3\) Install kubeadm kubelet kubectl
 
+apt-get install apt-transport-https -y
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
 
 apt-get update
-apt-get update && apt-get install kubeadm kubelet kubectl -y
+#apt-get install -y kubeadm kubelet kubectl
