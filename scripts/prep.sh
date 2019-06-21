a24b198549043fd6a0631a2284b2cf556ba7b07c
Gareth van der Berg <gareth.shaun@gmail.com>
2019-06-21 09:38:28 +0200

    update

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -14,7 +14,7 @@ cat <<EOF >/etc/docker/daemon.json
 EOF
 systemctl restart docker
 systemctl enable docker
-usermod $USER -aG docker
+usermod ${username} -aG docker
 
 echo
 echo 2\) Disable Swap
@@ -27,17 +27,17 @@ echo 3\) Install dependencies, kubeadm kubelet kubectl
 echo
 
 apt-get install -y apt-transport-https
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+curl -s --proxy ${proxy} https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
 
 apt-get update
-apt-get install -y kubeadm kubelet kubectl
-apt-mark hold kubelet kubeadm kubectl
+apt-get install -y kubeadm #kubelet kubectl
+apt-mark hold kubeadm #kubelet kubectl
 
-echo
-echo 4\) Adding " cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory" to /boot/firmware/cmdline.txt
-echo
+# echo
+# echo 4\) Adding " cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory" to /boot/firmware/cmdline.txt
+# echo
 
-sudo cp /boot/firmware/cmdline.txt /boot/firmware/cmdline_backup.txt
-orig="$(head -n1 /boot/firmware/cmdline.txt) cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory"
-echo $orig | sudo tee /boot/firmware/cmdline.txt
+# sudo cp /boot/firmware/cmdline.txt /boot/firmware/cmdline_backup.txt
+# orig="$(head -n1 /boot/firmware/cmdline.txt) cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory"
+# echo $orig | sudo tee /boot/firmware/cmdline.txt
